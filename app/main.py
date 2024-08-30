eb71606f63b39cd1f30553cd944fcb3ea0e7c833
osawa-koki <koki@osawa.dev>
2024-08-30 21:39:45 +0900

    CICDワークフローの作成。

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-print("Hello, World!")
+from selenium import webdriver
+
+print("Starting the browser")
+driver = webdriver.Chrome()
+
+print("Browser started")
+driver.get("https://www.google.com")
+
+driver.save_screenshot("./dist/screenshot.png")
