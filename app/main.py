40dab27baea1df7e1d6375fc3193068ce37dd19f
osawa-koki <koki@osawa.dev>
2024-08-30 23:10:54 +0900

    small fix

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
 options.add_argument("--window-size=1920,1080")
+options.add_argument('--lang=ja-JP')
 
 count = 0
 
