58e1b169ed64034eac8af735debf71e053794ddb
osawa-koki <koki@osawa.dev>
2024-08-30 22:50:21 +0900

    Seleniumコードの実装。

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 options = Options()
 options.add_argument("--headless")
@@ -8,9 +9,37 @@ options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
 options.add_argument("--window-size=1920,1080")
 
+count = 0
+
+
+def screenshot(driver):
+    global count
+    count += 1
+    driver.save_screenshot(f"./dist/{count}.png")
+
+
 driver = webdriver.Chrome(options=options)
 
 driver.get("https://www.google.com")
-driver.save_screenshot("./dist/screenshot.png")
+screenshot(driver)
+
+q = driver.find_element(By.NAME, "q")
+q.send_keys("Selenium")
+screenshot(driver)
+
+search = driver.find_element(By.NAME, "btnK")
+search.click()
+screenshot(driver)
+
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+screenshot(driver)
+
+next_page = driver.find_element(By.LINK_TEXT, "2")
+next_page.click()
+screenshot(driver)
+
+images = driver.find_element(By.LINK_TEXT, "画像")
+images.click()
+screenshot(driver)
 
 driver.quit()
